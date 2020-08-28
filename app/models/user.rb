a167e9872fcf0fd863c46f4527fa904dd3cce976
ankh-miyata <r-miyata@ankh-systems.co.jp>
2020-08-28 05:51:58 +0000

    Add password minimun limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
                        uniqueness: true
                        
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
+
 
 
