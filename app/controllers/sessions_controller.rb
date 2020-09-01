3e45c5cbc0f61b350f38e7dc20ec93f5da7b2a62
ankh-miyata <r-miyata@ankh-systems.co.jp>
2020-09-01 05:47:06 +0000

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       redirect_to user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
